ef9db377531fe0a91f194b035df152a5334bbe20
Alexander Parshin <parshin.alexander@gmail.com>
2015-02-09 01:07:53 +0300

    Add chaining to gmxDeferred

--- src/Deferred.js
+++ src/Deferred.js
@@ -1,4 +1,9 @@
-﻿var gmxDeferred = function(cancelFunc) {
+﻿//all the methods can be called without instance itself
+//For example:
+//
+// var def = new gmxDeferred();
+// doSomething(def.resolve) (instead of doSomething(def.resolve.bind(def))
+var gmxDeferred = function(cancelFunc) {
     var resolveCallbacks = [],
         rejectCallbacks = [],
         isFulfilled = false,
@@ -6,7 +11,7 @@
         fulfilledData,
         onceAdded = false;
         
-    var _fulfill = function(resolved) {
+    var fulfill = this._fulfill = function(resolved) {
         if (isFulfilled) {
             return;
         }
@@ -20,35 +25,47 @@
     }
     
     this.resolve = function(/*data*/) {
-        _fulfill.apply(null, [true].concat([].slice.call(arguments)));
+        fulfill.apply(null, [true].concat([].slice.call(arguments)));
     }
     
     this.reject = function(/*data*/) {
-        _fulfill.apply(null, [false].concat([].slice.call(arguments)));
+        fulfill.apply(null, [false].concat([].slice.call(arguments)));
     }
     
-    this.then = function(resolveCallback, rejectCallback) {
-        if (isFulfilled) {
-            if (isResolved) {
-                resolveCallback && resolveCallback.apply(null, fulfilledData);
-            } else {
-                rejectCallback && rejectCallback.apply(null, fulfilledData);
+    var then = this.then = function(resolveCallback, rejectCallback) {
+        var def = new gmxDeferred(),
+            fulfillFunc = function(func, resolved) {
+                return function() {
+                    if (!func) {
+                        def._fulfill.apply([resolved].concat([].slice.call(arguments)));
+                    } else {
+                        var res = func.apply(null, arguments);
+                        if (res instanceof gmxDeferred) {
+                            res.then(def.resolve, def.reject)
+                        } else {
+                            def.resolve(res);
+                        }
+                    }
+                }
             }
+        if (isFulfilled) {
+            fulfillFunc(isResolved ? resolveCallback : rejectCallback, isResolved).apply(null, fulfilledData)
         } else {
-            resolveCallback && resolveCallbacks.push(resolveCallback);
-            rejectCallback && rejectCallbacks.push(rejectCallback);
+            resolveCallbacks.push(fulfillFunc(resolveCallback, true));
+            rejectCallbacks.push(fulfillFunc(rejectCallback, false));
         }
+        return def;
     }
     
     this.once = function(onceResolveCallback) {
         if (!onceAdded) {
             onceAdded = true;
-            this.then(onceResolveCallback);
+            then(onceResolveCallback);
         }
     }
     
     this.always = function(callback) {
-        this.then(callback, callback);
+        then(callback, callback);
     }
     
     this.getFulfilledData = function() {
