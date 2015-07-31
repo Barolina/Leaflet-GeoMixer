5975424a3a212d25c57db162bbbaebdb0a731b0b
OriginalSin <saleks@scanex.ru>
2015-07-31 21:44:26 +0300

    distVincenty return float type

--- src/gmxAPIutils.js
+++ src/gmxAPIutils.js
@@ -761,8 +761,8 @@ var gmxAPIutils = {
         return ang * (Math.PI / 180.0);
     },
 
-	distVincenty: function(lon1, lat1, lon2, lat2) {
-		var p1 = {
+    distVincenty: function(lon1, lat1, lon2, lat2) {
+        var p1 = {
             lon: gmxAPIutils.degRad(lon1),
             lat: gmxAPIutils.degRad(lat1)
         },
@@ -782,35 +782,35 @@ var gmxAPIutils = {
             lambda = L1,
             lambdaP = 2 * Math.PI,
             iterLimit = 20;
-		while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0) {
-				var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda),
+        while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0) {
+                var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda),
                     sinSigma = Math.sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) +
-					(cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
-				if (sinSigma === 0) { return 0; }
-				var cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda,
+                    (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) * (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
+                if (sinSigma === 0) { return 0; }
+                var cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda,
                     sigma = Math.atan2(sinSigma, cosSigma),
                     sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma,
                     cosSqAlpha = 1 - sinAlpha * sinAlpha,
                     cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
-				if (isNaN(cos2SigmaM)) { cos2SigmaM = 0; }
-				var C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
-				lambdaP = lambda;
-				lambda = L1 + (1 - C) * f * sinAlpha *
-					(sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
-		}
-		if (iterLimit === 0) { return NaN; }
+                if (isNaN(cos2SigmaM)) { cos2SigmaM = 0; }
+                var C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
+                lambdaP = lambda;
+                lambda = L1 + (1 - C) * f * sinAlpha *
+                    (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));
+        }
+        if (iterLimit === 0) { return NaN; }
 
-		var uSq = cosSqAlpha * ((a * a) / (b * b) - 1),
-		//var uSq = cosSqAlpha * (a * a - b * b) / (b*b),
+        var uSq = cosSqAlpha * ((a * a) / (b * b) - 1),
+        //var uSq = cosSqAlpha * (a * a - b * b) / (b*b),
             A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))),
             B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq))),
             deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) -
-				B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM))),
+                B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM))),
             s = b * A * (sigma - deltaSigma);
 
-		s = s.toFixed(3);
-		return s;
-	},
+        //s = s.toFixed(3);
+        return s;
+    },
 
     /** Get point coordinates from string
      * @memberof L.gmxUtil
