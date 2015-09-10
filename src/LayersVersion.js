f2cc565880df3a66a4851a650fbd677993e8e9ae
Alexander Parshin <parshin.alexander@gmail.com>
2015-09-10 12:08:52 +0300

    Update raw properties during layer version update

--- src/LayersVersion.js
+++ src/LayersVersion.js
@@ -131,7 +131,7 @@ L.gmx.VectorLayer.include({
         if (layerDescription) {
             var gmx = this._gmx;
             if (layerDescription.properties) {
-                gmx.properties = layerDescription.properties;
+                gmx.properties = gmx.rawProperties = layerDescription.properties;
                 gmx.dataManager.updateVersion();
                 this.fire('versionchange');
             }
