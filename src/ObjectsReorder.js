cb4470d053a74b9f9e1081a5c6f8ac519ba8df89
OriginalSin <saleks@scanex.ru>
2015-12-10 11:52:05 +0300

    setSortFunc refactoring

--- src/ObjectsReorder.js
+++ src/ObjectsReorder.js
@@ -8,6 +8,7 @@ var MAX = 1000000;
 L.gmx.VectorLayer.include({
     _objectsReorder: {
         all: {},
+        userSetSortFunc: false,     // user sort func flag 
         sortFunc: null,
         count: 0,
         addToReorder: function (id, bottomFlag) {
@@ -22,19 +23,33 @@ L.gmx.VectorLayer.include({
                 this.redrawItem(id);
             }
         },
+        sortItems: function(a, b) {     // layer context
+            var reorder = this._objectsReorder;
+            if (reorder.count > 0) {
+                var ap = reorder.all[a.id],
+                    bp = reorder.all[b.id];
+
+                if (ap || bp) {
+                    ap = ap ? ap + (ap > 0 ? MAX : -MAX) : 0;
+                    bp = bp ? bp + (bp > 0 ? MAX : -MAX) : 0;
+                    return ap - bp;
+                }
+            }
+            return reorder.sortFunc ? reorder.sortFunc.call(this, a, b) : 0;
+        },
         resetSortFunc: function (layer) {
-            var zIndexField = layer._gmx.zIndexField;
-            layer.setSortFunc(
-                zIndexField ?
-                function(a, b) {
-                    var res = Number(a.properties[zIndexField]) - Number(b.properties[zIndexField]);
+            var reorder = layer._objectsReorder;
+            layer._gmx.sortItems = reorder.sortItems;
+            reorder.sortFunc = layer._gmx.zIndexField && !reorder.userSetSortFunc ?
+                function(a, b) {    // layer context
+                    var res = Number(a.properties[this._gmx.zIndexField]) - Number(b.properties[this._gmx.zIndexField]);
                     return res ? res : a.id - b.id;
                 }
                 :
                 function(a, b) {
                     return a.id - b.id;
                 }
-            );
+            ;
         },
         onAdd: function (layer) {
             var gmx = layer._gmx;
@@ -71,11 +86,13 @@ L.gmx.VectorLayer.include({
     bringToTopItem: function (id) {
         this._objectsReorder.addToReorder(id);
         this.redrawItem(id);
+        return this;
     },
 
     bringToBottomItem: function (id) {
         this._objectsReorder.addToReorder(id, true);
         this.redrawItem(id);
+        return this;
     },
 
     clearReorderArrays: function () {
@@ -83,6 +100,7 @@ L.gmx.VectorLayer.include({
         reorder.all = {};
         reorder.count = 0;
         this.repaint();
+        return this;
     },
 
     setReorderArrays: function (top, bottom) {
@@ -90,38 +108,29 @@ L.gmx.VectorLayer.include({
         reorder.all = {};
         reorder.count = 0;
         bottom.forEach(function (id) { reorder.addToReorder(id, true); });
-        top.map(function (id) { reorder.addToReorder(id); });
+        top.forEach(function (id) { reorder.addToReorder(id); });
         this.repaint();
+        return this;
     },
 
     getSortedItems: function (arr) {
-        var reorder = this._objectsReorder;
-        return arr.sort(reorder.count > 0 ? this._gmx.sortItems : reorder.sortFunc);
+        return arr.sort(L.bind(this._objectsReorder.count > 0 ? this._gmx.sortItems : this._objectsReorder.sortFunc, this));
     },
 
     resetSortFunc: function () {
         this._objectsReorder.resetSortFunc(this);
+        return this;
     },
 
     setSortFunc: function (func) {
         var reorder = this._objectsReorder;
         reorder.sortFunc = func;
-        this._gmx.sortItems = function(a, b) {
-            if (reorder.count > 0) {
-                var ap = reorder.all[a.id],
-                    bp = reorder.all[b.id];
-
-                if (ap || bp) {
-                    ap = ap ? ap + (ap > 0 ? MAX : -MAX) : 0;
-                    bp = bp ? bp + (bp > 0 ? MAX : -MAX) : 0;
-                    return ap - bp;
-                }
-            }
-            return reorder.sortFunc ? reorder.sortFunc(a, b) : 0;
-        };
+        reorder.userSetSortFunc = func ? true : false;
+        this._gmx.sortItems = reorder.sortItems;
         this.repaint();
+        return this;
     },
-    disableFlip: function() { this._objectsReorder.disabled = true; },
-    enableFlip: function() { this._objectsReorder.disabled = false; }
+    disableFlip: function() { this._objectsReorder.disabled = true; return this; },
+    enableFlip: function() { this._objectsReorder.disabled = false; return this; }
 });
 })();
