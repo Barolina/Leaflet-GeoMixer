d9a6bb6031bb4b6e7b9e65b707b05a8b366c76aa
OriginalSin <saleks@scanex.ru>
2016-11-08 14:10:26 +0300

    Rename events like Leaflet map

--- src/GeomixerMap.js
+++ src/GeomixerMap.js
@@ -139,7 +139,7 @@ var gmxMap = L.Class.extend({
 		this.layers.push(layer);
 		this.layersByTitle[props.title] = layer;
 		this.layersByID[props.name] = layer;
-		this.fire('onAddLayer', {layer: layer});
+		this.fire('layeradd', {layer: layer});
 
 		return this;
 	},
@@ -156,7 +156,7 @@ var gmxMap = L.Class.extend({
 
 		delete this.layersByTitle[props.title];
 		delete this.layersByID[props.name];
-		this.fire('onRemoveLayer', {layer: layer});
+		this.fire('layerremove', {layer: layer});
 
 		return this;
 	},
